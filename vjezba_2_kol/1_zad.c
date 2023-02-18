75bed7126f3dfb9fb19d69b2ed41b0c3f0062989
ivorp95 <ivor.pangos@live.com>
2023-02-18 15:33:43 +0100

    vjezba_2_kol  folder 	new file:   1_zad.c

---
+++ vjezba_2_kol/1_zad.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+
+
+char ascii_znak(int broj);
+
+
+
+int main (){
+
+char ascii;
+int broj;
+
+printf(" int: ");
+scanf("%d", &broj);
+
+ascii=ascii_znak(broj);
+printf(" ascii: %c \n", ascii);
+
+    return 0;
+
+}
+
+
+
+char ascii_znak(int broj){
+
+if(broj>=65 && broj<=125 )
+return broj;
+
+else
+    return 63;
+
+
+}
