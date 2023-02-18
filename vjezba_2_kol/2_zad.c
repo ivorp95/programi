64cd31388ae03b932dd1822cd31ed3ba1e2f0d40
ivorp95 <ivor.pangos@live.com>
2023-02-18 19:24:22 +0100

    modified:   2_zad.c

--- vjezba_2_kol/2_zad.c
+++ vjezba_2_kol/2_zad.c
@@ -8,17 +8,17 @@
 int main (){
 
 float x, z;
-float* pbroj_1=&x, *pbroj_3=&z;       //pointeri za svoju vrijednost dobivaju adrese varijabli
+float* pbroj_1=&x, *pbroj_3=&z;       //pointeri za svoju vrijednost dobivaju adrese varijabli x y z
 
 int y;
 int* pbroj_2=&y;
 
 printf("unesi realni broj x: ");
-scanf("%f", &*pbroj_1);
+scanf("%f", &*pbroj_1);             //vrijednosti unesene scanf-om se spremaju na adrese koje cuvaju *pointer verijable
 printf("unesi cijeli broj y: ");
 scanf("%d", &*pbroj_2);
 
-*pbroj_3=cosf(*pbroj_1)-2*(*pbroj_2);
+*pbroj_3=cosf(*pbroj_1)-2*(*pbroj_2);   // dobivena vrijednost z se sprema na adresu koju cuva *pointer varijabla
 
 
 
