04b4440cc286af030d3e0f8388c6bd430cdf89ed
ivorp95 <ivor.pangos@live.com>
2023-02-19 08:18:58 +0100

    new file:   4_zad.c

---
+++ vjezba_2_kol/4_zad.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+
+int main (){
+
+FILE* pdatoteka;
+int i;
+
+pdatoteka=fopen("/users/ivorp/desktop/kontrolna_zadaca.txt","w");
+
+if (pdatoteka){
+printf("datoteka je kreirana\n");
+    for(i=1;i<=100;i++){
+        if(i%5==0)
+        fprintf(pdatoteka,"\t%d\n",i);
+
+    }
+    if(fclose(pdatoteka)==0)
+    printf("datoteka je zatvorena\n");
+}
+else {
+printf("datoteka nije kreirana\n");
+}
+
+    return 0;
+}
