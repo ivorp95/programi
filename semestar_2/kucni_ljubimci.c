2e573426f5129cb1181ed9e415ac824bc4963d9a
ivorp95 <ivor.pangos@live.com>
2023-03-13 21:11:59 +0100

    modified:   kucni_ljubimci.c

--- semestar_2/kucni_ljubimci.c
+++ semestar_2/kucni_ljubimci.c
@@ -18,26 +18,31 @@ int godine;
 kucni_ljubimci ljubimci[4], ljubimac_default = { "papiga" , "Konstantin",  "zuta", 5};
 
 for (i=0; i<4; i++){
-    printf("\n\tUnos podataka za %d. ljubimca ", i+1);
-    printf("\n Unseite vrstu:");
-    gets(ljubimci[i].vrsta);
+    printf("\tZa pocetak unosa pritisnite enter !!!");
+    scanf("%c",&enter);
+    printf("\n\n\n\n Unos podataka za %d. ljubimca \n", i+1);
+    printf("\n\n Unseite vrstu: ");
+    gets(ljubimci[i].vrsta);   // ??? googlati 
     
 
-    printf("\n Unseite Ime ljubimca:");
+    printf("\n Unseite Ime ljubimca: ");
     scanf("%30s",ljubimci[i].ime);
 
-    printf("\n Unseite boju:");
+    printf("\n Unseite boju: ");
     scanf("%30s",ljubimci[i].boja);
 
-    printf("\n Unseite vrstu:");
+    printf("\n Unseite godine: ");
     scanf("%d",&ljubimci[i].godine);
+}
 
+for(i=0;i<4;i++){
     if(DEBUG == 1){
-        printf("\n %d.Ljubimac - Vrsta: %s, Ime: %s , Boja: %s, Godine: %d", i+1, ljubimci[i].vrsta, ljubimci[i].ime, ljubimci[i].boja, ljubimci[i].godine);
+        printf("\n\n %d. Ljubimac \n\tVrsta: %s \n\tIme: %s  \n\tBoja: %s \n\tGodine: %d\n", i+1, ljubimci[i].vrsta, ljubimci[i].ime, ljubimci[i].boja, ljubimci[i].godine);
 
     }
 }
 
+printf("\n\n Default ljubimac \n\tVrsta: %s \n\tIme: %s  \n\tBoja: %s \n\tGodine: %d\n", ljubimac_default.vrsta, ljubimac_default.ime, ljubimac_default.boja, ljubimac_default.godine);
 
 
 
