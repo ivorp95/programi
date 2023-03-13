1ed2e4bca06c73e3079c15961e51e5edb3293cea
ivorp95 <ivor.pangos@live.com>
2023-03-13 22:50:03 +0100

    modified:   studenti_struct.c

--- semestar_2/studenti_struct.c
+++ semestar_2/studenti_struct.c
@@ -3,17 +3,64 @@
 #include <string.h>
 #define DEBUG 1
 
-int main(){
 typedef struct{
-    int jmbag,br_ocjena, god_rod;
+    int jmbag,br_ocjena,god_rod;
+    int ocjene[10];
+    float prosjek;
     char ime[21], prezime[21];
-    int ocjene;
 }student;
 
-int MAX;
+float prosjek_izracun(int ukupno, int ocjena[]);
+
+
+
+
+int main(){
+
+int i,j,MAX;
+char enter;
 student Student[MAX];
 
 printf("\nUnesite max kolicinu unosa: ");
 scanf("%d",&MAX);
 
+for (i=0;i<MAX;i++){
+    printf("\n\nUnesite Ime i prezime %d. studenta",i+1);
+    scanf("%20s %20s",Student[i].ime,Student[i].prezime);
+
+    printf("\nUnesite JMBAG %d. studenta",i+1);
+    scanf("%d",&Student[i].jmbag);
+
+    printf("\nUnesite Godinu rodenja %d. studenta",i+1);
+    scanf("%d",&Student[i].god_rod);
+
+    printf("\nUnesite ukupni broj ocjena %d. studenta",i+1);
+    scanf("%d",&Student[i].br_ocjena);
+
+        for (j=0;j<=Student[i].br_ocjena;j++){
+        printf("\n Unesite ocjenu za %d predmet ", j+1);
+        scanf("%d",&Student[i].ocjene[j]);
+        }
+
 }
+
+
+
+
+return 0;
+}
+
+
+
+
+
+float prosjek_izracun(int ukupno, int ocjena[]){
+int i;
+float suma, prosjek;
+for (i=0; i<=ukupno; i++)
+    suma+=ocjena[i];
+
+prosjek=suma/ukupno;
+
+return prosjek;
+};
