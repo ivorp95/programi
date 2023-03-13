391c27377b800fcf8ecc86b29a2cd5aa54b3afe2
ivorp95 <ivor.pangos@live.com>
2023-03-13 22:13:59 +0100

    new file:   studenti_struct.c

---
+++ semestar_2/studenti_struct.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#define DEBUG 1
+
+int main(){
+typedef struct{
+    int jmbag,br_ocjena, god_rod;
+    char ime[21], prezime[21];
+    int ocjene;
+}student;
+
+int MAX;
+student Student[MAX];
+
+printf("\nUnesite max kolicinu unosa: ");
+scanf("%d",&MAX);
+
+}
