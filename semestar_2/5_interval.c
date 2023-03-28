640f9e82005b6260494ef0ff44d5e63ad43bc2d3
ivorp95 <ivor.pangos@live.com>
2023-03-28 14:32:35 +0200

    modified:   5_interval.c

--- semestar_2/5_interval.c
+++ semestar_2/5_interval.c
@@ -18,16 +18,12 @@ typedef struct{
     
 void input(Interval new_element, int i_input, List* plist);
 void output(List* plist);
-
-/*
-void ispisi_v2(Lista lista);
-void obrisi(int iObrisi, Lista* plista);
-*/
+void check_numbers(List* plist, int low_limit, int up_limit);
 
 
 int main() {
     
-    int i,range_num;
+    int i,range_num, lower, upper;
 
     List list_interval;
     list_interval.i_last = -1;
@@ -41,6 +37,7 @@ int main() {
             printf("must be in the range 0 - 20");
     }while(range_num>MAX && range_num<=0);
 
+
     for (i = 0; i < range_num; i++) {
 
         printf("\n\t enter the %d. number: ",i+1);
@@ -50,32 +47,45 @@ int main() {
         input(input_num, i, &list_interval);
     }
 
-    ispisi(&lista_nastave);
+
+    do{
+        printf("\n\tenter the Interval limits - lower:");
+        scanf("%d", &lower);
+        printf("\n\tenter the Interval limits - upper:");
+        scanf("%d", &upper);
+
+            if(lower>=upper)
+            printf("\nlower limit must be less than upper");
+    }while(lower>=upper);
+
+
+
+    output(&list_interval);
     
     return 0;
 }
 
 
-void ubaci(Interval new_element, int i_input, List* plist){
+void input(Interval new_element, int i_input, List* plist){
     int i;
 
-    if (plista->iZadnji == MAX - 1)
-    printf("\n Lista je puna. ");
+    if (plist->i_last == MAX - 1)
+    printf("\n List is full. ");
 
-    else if (iUbaci > (plista->iZadnji + 1) || iUbaci < 0)
-    printf("\n Indeks ubacivanja iUbaci nije ispravno definiran. ");
+    else if (i_input > (plist->i_last + 1) || i_input < 0)
+    printf("\n Index is wrong ");
 
     else{
-        for (i = plista->iZadnji; i >= iUbaci; i--){
-        plista->elementi[i + 1] = plista->elementi[i];
+        for (i = plist->i_last; i >= i_input; i--){
+        plist->elements[i + 1] = plist->elements[i];
         }
-    plista->iZadnji++;
-    plista->elementi[iUbaci] = noviElement;
+    plist->i_last++;
+    plist->elements[i_input] = new_element;
     }
 }
 
 
-void ispisi(Lista* plista){
+void output(List* plist){
     int i;
     int uk_P = 0, uk_V = 0, uk_S = 0;
 
@@ -97,36 +107,15 @@ void ispisi(Lista* plista){
 
 }
 
-/*  
-void ispisi_v2(Lista lista)
-{
-int i;
 
-for (i = 0; i <= lista.iZadnji; i++)
-{
-printf("\n\n %d. ELEMENT LISTE ", i + 1);
-printf("\n\t ime: %s ", lista.elementi[i].ime);
-printf("\n\t vrsta: %s ", lista.elementi[i].vrsta);
-printf("\n\t dob: %d ", lista.elementi[i].dob);
-}
+void check_numbers(List* plist, int low_limit, int up_limit){
+
+
+
+
 }
 
 
 
-void obrisi(int iObrisi, Lista* plista)
-{
-int i;
 
-if (iObrisi > plista->iZadnji || iObrisi < 0)
-printf("\n Indeks brisanja iObrisi nije ispravno definiran. ");
 
-else
-{
-for (i = iObrisi; i < plista->iZadnji; i++)
-{
-plista->elementi[i] = plista->elementi[i + 1];
-}
-plista->iZadnji--;
-}
-}
-*/
