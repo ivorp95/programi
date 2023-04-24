103d4e8a67563b6e3cf8f418e513076cba4b45a3
ivorp95 <ivor.pangos@live.com>
2023-04-24 20:11:47 +0200

    new file:   8_natjecatelj_pred.c

---
+++ semestar_2/8_natjecatelj_pred.c
@@ -0,0 +1,172 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<malloc.h>
+
+
+typedef struct
+{
+	char ime[21];
+	char spol;
+	int godRodj;
+} Natjecatelj;
+
+typedef struct Celija
+{
+	Natjecatelj element;
+	struct Celija* psljedeca;
+} CelijaNatjecatelj;
+
+
+typedef struct
+{
+	CelijaNatjecatelj* pizlaz;
+	CelijaNatjecatelj* pulaz;
+} RedNatjecatelj;
+
+
+void unosNatjecatelja(Natjecatelj* pnatjecatelj);
+
+void ubaci(Natjecatelj noviElement, RedNatjecatelj* pred);
+void ispisi(RedNatjecatelj* pred);
+void obrisi(RedNatjecatelj* pred);
+void izmijeni(Natjecatelj izmijenjeniElement, Natjecatelj originalniElement, RedNatjecatelj* pred);
+CelijaNatjecatelj* pronadjiAdresu_ime(char* imeNatjecatelja, RedNatjecatelj* pred);
+
+
+int main() {
+
+	RedNatjecatelj red_natjecatelja;
+	red_natjecatelja.pulaz =(CelijaNatjecatelj*)malloc(sizeof(CelijaNatjecatelj));
+	red_natjecatelja.pulaz->psljedeca = NULL;
+	red_natjecatelja.pizlaz = mojiLjubimci.pulaz;
+
+	Natjecatelj natjecatelj;
+
+	char noviUnos;
+	char imeObrisi[21];
+	int i;
+
+
+	do {
+		unosNatjecatelja(&natjecatelj);
+
+		ubaci(natjecatelj, &red_natjecatelja);
+
+		printf("\n\n Zelite li unijeti podatke za novog natjecatelja? (D/N) ");
+		scanf_s(" %c", &noviUnos, 1);
+	} while (noviUnos == 'D');
+
+	ispisi(&red_natjecatelja);
+	printf("\n\n");
+
+
+	obrisi(&red_natjecatelja);
+	printf("\n\n\n\n ... Ispis celija reda nakon 1. brisanja elementa ... ");
+	ispisi(&red_natjecatelja);
+
+	obrisi(&red_natjecatelja);
+	printf("\n\n\n\n ... Ispis celija reda nakon 2. brisanja elementa ... ");
+	ispisi(&red_natjecatelja);
+
+
+	ispisi(&red_natjecatelja);
+	
+	printf("\n\n");
+
+	return 0;
+}
+
+
+void unosNatjecatelja(Natjecatelj* pnatjecatelj)
+{
+	printf("\n Unesite ime natjecatelja: ");
+	scanf_s("%20s", pnatjecatelj->ime, 21);
+
+	do {
+		printf(" Unesite spol natjecatelja: ");
+		scanf_s(" %c", &pnatjecatelj->spol, 1);
+
+		if (pnatjecatelj->spol != 'Z' && pnatjecatelj->spol != 'M')
+			printf("\t Spol treba biti Z ili M.\n");
+	} while (pnatjecatelj->spol != 'Z' && pnatjecatelj->spol != 'M');
+
+	do {
+		printf(" Unesite godinu rodjenja natjecatelja: ");
+		scanf_s("%d", &pnatjecatelj->godRodj);
+
+		if (pnatjecatelj->godRodj < 1930 || pnatjecatelj->godRodj > 2005)
+			printf("\t Godina rodjenja treba biti u intervalu od 1930 do 2005.\n");
+	} while (pnatjecatelj->godRodj < 1930 || pnatjecatelj->godRodj > 2005);
+}
+
+
+
+void ubaci(Natjecatelj noviElement, RedNatjecatelj* pred)
+{
+	pred->pulaz->psljedeca = (CelijaNatjecatelj*)malloc(sizeof(CelijaNatjecatelj));
+	pred->pulaz = pred->pulaz->psljedeca;
+
+	pred->pulaz->element = noviElement;
+	pred->pulaz->psljedeca = NULL;
+}
+
+
+void ispisi(RedNatjecatelj* pred)
+{
+	CelijaNatjecatelj* ptrenutnaCelija = pred->pizlaz;
+
+	int brZ = 0, brM = 0;
+
+	while (ptrenutnaCelija->psljedeca != NULL)
+	{
+		ptrenutnaCelija = ptrenutnaCelija->psljedeca;
+		printf("\n");
+		printf("\n Ime: %s ", ptrenutnaCelija->element.ime);
+		printf("\n Spol: %c ", ptrenutnaCelija->element.spol);
+		printf("\n Godina rodjenja: %d ", ptrenutnaCelija->element.godRodj);
+
+		if (ptrenutnaCelija->element.spol == 'Z')
+			brZ++;
+		else
+			brM++;
+	}
+
+	printf("\n\n UKUPNO NATJECATELJA - Zenskih: %d; Muskih: %d.", brZ, brM);
+}
+
+
+void obrisi(RedNatjecatelj* pred)
+{
+	CelijaNatjecatelj* pcelijaZaBrisanje;
+
+	if (pred->pizlaz == pred->pulaz)
+	{
+		printf("\n Red je prazan. ");
+	}
+	else
+	{
+		pcelijaZaBrisanje = pred->pizlaz;
+		pred->pizlaz = pred->pizlaz->psljedeca;
+		free(pcelijaZaBrisanje);
+	}
+}
+
+
+void izmijeni(Natjecatelj izmijenjeniElement, Natjecatelj originalniElement, RedNatjecatelj* pred)
+{
+	CelijaNatjecatelj* ptrenutnaCelija = pred->pizlaz;
+
+	while (ptrenutnaCelija->psljedeca != NULL)
+	{
+		ptrenutnaCelija = ptrenutnaCelija->psljedeca;
+
+		if (strcmp(ptrenutnaCelija->element.ime, originalniElement.ime) == 0 &&
+			strcmp(ptrenutnaCelija->element.spol, originalniElement.spol) == 0 &&
+			ptrenutnaCelija->element.godRodj == originalniElement.godRodj)
+		{
+			ptrenutnaCelija->element = izmijenjeniElement;
+		}
+	}
+}
+
