64a350fa23eaa752ef2a53d953b5f53bda0ce95c
ivorp95 <ivor.pangos@live.com>
2023-05-29 20:57:32 +0200

    new file:   asp_kolokvi/1kol_zadatak1_listaPointer.c 	new file:   asp_kolokvi/1kol_zadatak2_stogPolje.c

---
+++ semestar_2/asp_kolokvi/1kol_zadatak1_listaPointer.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+
+typedef struct {
+	float cijena;
+	char proizvodjac[21];
+	char tvrdoca[3];
+}Olovka;
+
+typedef struct Celija {
+	Olovka element;
+	struct Celija* sljedeca;
+} Lista;
+
+Lista* zadnjaCelija(Lista* pokLista);
+void ubaci(Olovka x, Lista* pozicija_ubacivanja);
+void ispisOlovke(Lista* lista);
+Lista* adresaCelije(Lista* lista, int pozicija);
+void obrisi(Lista* pozicija_brisanja);
+
+int pronadjiTvrdocu(char trazeniPodatak[3], Lista* pokLista);
+
+
+int main() {
+
+	Lista* mojaLista;
+	mojaLista = (Lista*)malloc(sizeof(Lista));
+	mojaLista->sljedeca = NULL;
+	Olovka unos;
+	int i, brojOlovki;
+	char odgovor, traziOlovku[3];
+
+	do {
+		printf("\n\n\n Unesite podatke za jednu olovku");
+		printf("\n Unesite proizvodjaca olovke: ");
+		scanf_s("%20s", unos.proizvodjac, 21);
+
+		do {
+			printf("\n Unesite cijenu olovke: ");
+			scanf_s("%f", &unos.cijena);
+			if (unos.cijena <= 0)
+				printf("\n Cijena mora biti veca od 0");
+		} while (unos.cijena <= 0);
+
+		printf("\n Unesite tvrdocu olovke: ");
+		scanf_s("%2s", unos.tvrdoca,3);
+
+		ubaci(unos,zadnjaCelija(mojaLista));
+
+		printf("\n Zelite li unjeti podatke za jos jednu olovku 'D' ili 'N': ");
+		scanf_s(" %c", &odgovor, 1);
+
+	} while (odgovor == 'D');
+
+	ispisOlovke(mojaLista);
+
+	printf("\n Koju tvrdocu olovke zelite pronaci: ");
+	scanf_s(" %s", traziOlovku, 3);
+
+	brojOlovki = pronadjiTvrdocu(traziOlovku, mojaLista);
+
+	printf("\n U listi se nalazi %d olovki tvrdoce %s ", brojOlovki, traziOlovku);
+
+
+	return 0;
+};
+
+
+Lista* zadnjaCelija(Lista* pokLista) {
+	Lista* celija;
+	celija = pokLista;
+	do {
+		celija = celija->sljedeca;
+		if (celija == NULL) return pokLista;
+		if (celija->sljedeca == NULL) return celija;
+	} while (celija->sljedeca != NULL);
+}
+
+void ubaci(Olovka x, Lista* pozicija_ubacivanja) {
+	Lista* privremeno;
+	privremeno = pozicija_ubacivanja->sljedeca;
+	pozicija_ubacivanja->sljedeca = (Lista*)malloc(sizeof(Lista));
+	pozicija_ubacivanja->sljedeca->element = x;
+	pozicija_ubacivanja->sljedeca->sljedeca = privremeno;
+}
+
+void ispisOlovke(Lista* lista) {
+	Lista* celija;
+	celija = lista;
+	int i=0;
+	printf("\nIspis olovki:\n");
+	while (celija->sljedeca != NULL) {
+		celija = celija->sljedeca;
+		printf("\n\t\t%d.Olovka\t\t\t %s - %s ( %.2f eura)",i+1, celija->element.proizvodjac, celija->element.tvrdoca, celija->element.cijena);
+		i++;
+	}
+}
+
+Lista* adresaCelije(Lista* lista, int pozicija) {
+	Lista* celija;
+	celija = lista;
+	int brojac = 0;
+	while (celija->sljedeca != NULL) {
+		brojac = brojac + 1;
+		if (brojac == pozicija) {
+			break;
+		}
+		celija = celija->sljedeca;
+	}
+	return celija;
+}
+
+void obrisi(Lista* pozicija_brisanja) {
+	Lista* privremeno;
+	privremeno = pozicija_brisanja->sljedeca;
+	pozicija_brisanja->sljedeca = pozicija_brisanja->sljedeca->sljedeca;
+	free(privremeno);
+}
+
+
+int pronadjiTvrdocu(char trazeniPodatak[3], Lista* lista){
+	Lista* celija;
+	celija = lista;
+	int brojac=0;
+	printf("\nOlovke tvrdoce %s...\n",trazeniPodatak);
+	
+	while (celija->sljedeca != NULL) {
+		celija = celija->sljedeca;
+		if (strcmp(trazeniPodatak, celija->element.tvrdoca) == 0){
+			printf("\n\t\t %s", celija->element.tvrdoca);
+			brojac++;
+		}
+		else
+			printf("\n U listi nema olovke te tvrdoce");
+	}
+	return brojac;
+}
---
+++ semestar_2/asp_kolokvi/1kol_zadatak2_stogPolje.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+#define MAX 10
+
+
+typedef struct {
+	float visina;
+	int tezina;
+}Kutija;
+
+typedef struct {
+	int vrh;
+	Kutija elementi[MAX];
+} Stog;
+
+void ubaci(Kutija x, Stog* pokStog);
+int obrisiKutiju(Stog* pokStog);
+void ispis(Stog* pokStog);
+
+
+
+int main() {
+	Stog  mojStog;
+	mojStog.vrh = MAX;
+	Kutija unos;
+	int i, tezinaUkloni=0, tezinaPovrat=0;
+	char odgovor[10];
+
+	for (i = 0; i < MAX;i++) {
+		printf("\n\n\n Unesite podatke za %d kutiju", i + 1);
+		printf("\n Visina: ");
+		scanf_s("%f", &unos.visina);
+		printf("\n Tezina: ");
+		scanf_s("%d", &unos.tezina);
+
+		ubaci(unos, &mojStog);
+	
+
+		printf("\n Zelite li unjeti podatke za jos jednu Kutiju 'zelim': ");
+		scanf_s(" %s", odgovor, 10);
+
+		if (strcmp("zelim", odgovor) != 0)
+			break;
+	} ;
+
+	printf("\n Koliku ukupnu tezinu zelite ukloniti sa stoga? ");
+	scanf_s("%d", &tezinaUkloni);
+
+	do {
+		tezinaPovrat += obrisiKutiju(&mojStog);
+	} while (tezinaPovrat <= tezinaUkloni);
+
+
+
+	return 0;
+};
+
+
+
+void ubaci(Kutija x, Stog* pokStog) {
+	if (pokStog->vrh == 0)
+		printf("Stog je pun!");
+	else {
+		pokStog->vrh--;
+		pokStog->elementi[pokStog->vrh] = x;
+	}
+}
+
+void ispis(Stog* pokStog) {
+	int indeks;
+	printf("\nStog:\n");
+
+	for (indeks = pokStog->vrh; indeks <= MAX - 1; indeks++) {
+		printf("%", pokStog->elementi[indeks]);
+
+	}
+}
+
+int obrisiKutiju(Stog* pokStog) {
+	int i=0,ukupnaTezina=0;
+
+	if (pokStog->vrh == MAX)
+		printf("Stog je prazan!");
+	else
+		printf("\n Uklonjeno je : %d kg.", pokStog->elementi[pokStog->vrh].tezina);
+		ukupnaTezina = pokStog->elementi[pokStog->vrh].tezina;
+		pokStog->vrh++;
+
+
+	return ukupnaTezina;
+
+}
