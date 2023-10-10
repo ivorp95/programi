1dc9af0f9411566674fc8dcf6d90cbf6e486642a
ivorp95 <ivor.pangos@live.com>
2023-10-10 19:05:31 +0200

    modified:   faktorijel_vjezba/faktorijel_app.java

--- semestar_3/faktorijel_vjezba/faktorijel_app.java
+++ semestar_3/faktorijel_vjezba/faktorijel_app.java
@@ -3,7 +3,7 @@ package faktorijel_vjezba;
 public class faktorijel_app {
     
     public static void main(String[] args){
-        int vrijednost=5;
+        int vrijednost=11;
         Broj broj= new Broj(vrijednost);
         System.out.println("\n\tfaktorijel broja:\t"+vrijednost+"\tje:\t"+broj.faktorijel());
         
