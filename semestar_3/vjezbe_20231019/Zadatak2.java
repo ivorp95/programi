c09fad17a441438f28087edd2147de3eaf11956c
ivorp95 <ivor.pangos@live.com>
2023-10-19 17:11:50 +0200

    new file:   vjezbe_20231019/Zadatak2.java

---
+++ semestar_3/vjezbe_20231019/Zadatak2.java
@@ -0,0 +1,26 @@
+package vjezbe_20231019;
+
+import java.util.Scanner;
+
+public class Zadatak2 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int polje[]=new int[10];
+        int min=0;
+
+        for (int i=0; i<10;i++){
+            System.out.println("Unesite "+(i+1)+". broj: ");
+            polje[i]=sc.nextInt();
+        }
+
+        min=polje[0];
+        
+        for(int i=0;i<10;i++){
+            if(polje[i]<min){
+                min=polje[i];
+            }
+        }
+
+        System.out.println("Minimalni broj je :"+min);
+    }
+}
