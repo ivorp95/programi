35c3c38857fb88363a8d5c432ab3856a77d49cb0
ivorp95 <ivor.pangos@live.com>
2023-10-19 17:43:43 +0200

    new file:   vjezbe_20231019/Zadatak3.java 	new file:   vjezbe_20231019/Zadatak4.java 	new file:   vjezbe_20231019/Zadatak5.java 	new file:   vjezbe_20231019/Zadatak6.java

---
+++ semestar_3/vjezbe_20231019/Zadatak3.java
@@ -0,0 +1,27 @@
+package vjezbe_20231019;
+
+import java.util.Scanner;
+
+public class Zadatak3 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        
+        String zankovi;
+        int brojac=0;
+
+        System.out.print("Unesi recenicu: ");
+        zankovi=sc.nextLine();  //sc.nextLine  za unos stringa
+
+        // metoda 'contains' za utvrdjivanje postojanja znaga
+
+        //charAt ide po indexu stringa
+
+        for (int i=0;i<zankovi.length();i++){
+
+            if (zankovi.charAt(i)=='a'){
+                brojac++;
+                }
+        }
+        System.out.println(brojac);
+    }
+}
---
+++ semestar_3/vjezbe_20231019/Zadatak4.java
@@ -0,0 +1,20 @@
+package vjezbe_20231019;
+
+//zadatak 24.   pojavljuje li se veliko ili malo slovo
+
+
+import java.util.Scanner;
+public class Zadatak4 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String rijec;
+
+        System.out.println("Unesite rijec: ");
+        rijec=sc.nextLine();
+
+    
+
+
+
+    }
+}
