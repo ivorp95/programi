589661724c53b2abd730500a912fef3a4ad957cd
ivorp95 <ivpan@proton.me>
2024-04-18 00:46:45 +0200

    modified:   main.c

--- semestar_4/Procesi_verZaStudente/main.c
+++ semestar_4/Procesi_verZaStudente/main.c
@@ -20,19 +20,20 @@ int main()
 	int brP;
 	
 	unosProcesa(procesi, &brP);
-	
+	/*
 	inicijalizacijaVrijednostiProcesa(procesi, brP);
 	FCFSalgoritam(procesi, brP, &FCFS);
 	ispisIzlaznihPodataka(procesi, brP, FCFS, "FCFS");
 
 	printf("\n\n");
-		/*
+	*/
+
 	inicijalizacijaVrijednostiProcesa(procesi, brP);
 	SJFalgoritam_npe(procesi, brP, &SJFnpe);
 	ispisIzlaznihPodataka(procesi, brP, SJFnpe, "SJF-npe");
 
 	printf("\n\n");
-
+/*
 	inicijalizacijaVrijednostiProcesa(procesi, brP);
 	SJFalgoritam_pe(procesi, brP, &SJFpe);
 	ispisIzlaznihPodataka(procesi, brP, SJFpe, "SJF-pe");
