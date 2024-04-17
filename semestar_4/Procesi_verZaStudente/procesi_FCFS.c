55a7e67382c9959a2dcb558597596646a6660906
ivorp95 <ivpan@proton.me>
2024-04-17 20:19:56 +0200

    modified:   Procesi_verZaStudente/procesi_FCFS.c

--- semestar_4/Procesi_verZaStudente/procesi_FCFS.c
+++ semestar_4/Procesi_verZaStudente/procesi_FCFS.c
@@ -17,9 +17,12 @@ int sljedeciProcesFCFS(Proces* procesi, int brP, int tCurr)
 	for (i = 0; i < brP; i++)
 	{
 		if (procesi[i].flag == 0 && procesi[i].arrivalTime <= tCurr && procesi[i].arrivalTime < atMin)
+		//ako proces nije obraden i ako je vrijeme dolaska manje ili jednako trenutnom i 
+		//ako je vrijeme dolaska manje od atMin koji je na pocetku INT_MAX
 		{
-			indeks = i;
-			atMin = procesi[i].arrivalTime;
+			indeks = i; //index dobiva vrijednost i koji se salje funkciji FCFS kao broj procesa koji se obradjuje
+			atMin = procesi[i].arrivalTime; //atMin dobiva vrijednost vremena dolaska procesa pa u sljejdecoj provjeri uvjeta 
+											//nije zadovoljeno i FCFS funkciji se vraca index kao index seljedeceg procesa
 		}
 	}
 
@@ -36,28 +39,29 @@ void FCFSalgoritam(Proces* procesi, int brP, Algoritam* palgoritam)
 	{
 		indeks = sljedeciProcesFCFS(procesi, brP, tCurr);
 
-		if (indeks != -1)
+		if (indeks != -1) //index kojeg vraca funkcija sljedeciProces default je -1,
+						//u tom slucaju vrijeme se mora povecati jer nije dosao niti jedan proces
 		{
 			procesi[indeks].startTime = tCurr;
 			procesi[indeks].completionTime = tCurr + procesi[indeks].burstTime;
 			procesi[indeks].turnaroundTime = procesi[indeks].completionTime - procesi[indeks].arrivalTime;
 			procesi[indeks].waitingTime = procesi[indeks].turnaroundTime - procesi[indeks].burstTime;
-			procesi[indeks].flag = 1;
-			procesi[indeks].order = i + 1;
+			procesi[indeks].flag = 1; //proces uspjesno odradjen
+			procesi[indeks].order = i + 1; //index +1 redosljed kojim je zavrsen proces za index 0 order je 1 jer je to prvi proces
 
-			tt_suma += procesi[indeks].turnaroundTime;
-			wt_suma += procesi[indeks].waitingTime;
+			tt_suma += procesi[indeks].turnaroundTime; //dodavanje u sumu vrijednosti TT za izracun prosjeka na kraju
+			wt_suma += procesi[indeks].waitingTime;		//isto kao gore za prosjek WT
 
-			tCurr += procesi[indeks].burstTime;
+			tCurr += procesi[indeks].burstTime; //sljedece vrijeme je vrijeme nakon zavrsetka procesa = trenutno + burst ovog procesa
 			i++;
 		}
 		else
 		{
-			tCurr++;
+			tCurr++; //ako nije odraden prosli uvjet povecava se vrijeme i trazi novi index procesa korz gornju funkciju
 		}
 	}
 
-	palgoritam->att = (float)tt_suma / brP;
-	palgoritam->awt = (float)wt_suma / brP;
+	palgoritam->att = (float)tt_suma / brP; //prosjek TT -->  sumaTT / ukupan broj procesa
+	palgoritam->awt = (float)wt_suma / brP; //prosjek WT -->  sumaWT / ukupan broj procesa  i castanje u FLOAT jer je prosjek
 }
 
