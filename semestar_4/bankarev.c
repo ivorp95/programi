4440505e6bca83e7cd0e922498a962b7d159cbdc
ivorp95 <ivpan@proton.me>
2024-04-17 15:42:32 +0200

    modified:   bankarev.c

--- semestar_4/bankarev.c
+++ semestar_4/bankarev.c
@@ -10,8 +10,8 @@ typedef struct
 	char pID[4];
 	int maxRes[RES];
 	int allocatedRes[RES];
-	int flag;
-	int order;
+	int flag; //ako je proces zavrsen je 1 inace 0
+	int order; //redni broj kada je proces zavrsen inace -1
 } Proces;
 
 void unosResursa(int* numRes);
@@ -89,8 +89,8 @@ void unosProcesa(Proces* procesi, int* pbrP)
 			} while (procesi[i].maxRes[j] < procesi[i].allocatedRes[j]);
 		}
 
-		procesi[i].flag = 0;
-		procesi[i].order = -1;
+		procesi[i].flag = 0; //inicijalizacija za svaki proces flag 0
+		procesi[i].order = -1; //order -1
 	}
 }
 
@@ -102,62 +102,64 @@ void bankarevAlgoritam(Proces* procesi, int brP, int* numRes)
 
 	for (j = 0; j < RES; j++)
 	{
-		availableRes[j] = numRes[j];
+		availableRes[j] = numRes[j]; //dodjela resursa u pocetku svi imaju ono sto je dodjeljeno u inicijalizaciji
 
 		for (i = 0; i < brP; i++)
 		{
-			availableRes[j] -= procesi[i].allocatedRes[j];
+			availableRes[j] -= procesi[i].allocatedRes[j]; //petlja za svaki tip resursa oduzimamo alocirane od dostupnih i tako dobivamo koliko svaki ima dodjeljenih
 		}
 	}
 
+	
+	//petlja za obradu resursa
 	i = 0;
-	while (brProlazaka <= brP)
+	while (brProlazaka <= brP) //broj prolazaka manji/jednak od broja procesa
 	{
-		if (procesi[i].flag == 0)
+		if (procesi[i].flag == 0) //flag 0 znaci da proces nije zavrsen
 		{
-			ok = 0;
+			ok = 0; //ok provjera ako imamo dovoljno resursa za obradu procesa
 			for (j = 0; j < RES; j++)
 			{
 				if (procesi[i].allocatedRes[j] + availableRes[j] >= procesi[i].maxRes[j])
 				{
-					ok++;
+					ok++; //ako ima dovoljno resursa ok dobiva 1  i tako za svaki resurs pojedinacno
 				}	
 			}
 
-			if (ok == RES)
+			if (ok == RES) //kada petlja pije prode korz sve tipove resursa i ok dobije vrijednost RES (3 u ovom slucaju)
 			{
-				indeks = i;
-				procesi[indeks].order = ++order;
-				procesi[indeks].flag = 1;
+				indeks = i; //indeks dobije vriednost trenutnog i
+				procesi[indeks].order = ++order; //proces koji je obraden dobije u svoj order vrijednost ordera iz funkcije +1, order iz funkcije se povecava za jedan nakon obradenog procesa
+				procesi[indeks].flag = 1; //proces dobiva flag 1 jer je gotov jer smo nasli sve resurse za obradu
 
 				for (j = 0; j < RES; j++)
 				{
-					availableRes[j] = availableRes[j] + procesi[indeks].allocatedRes[j];
+					availableRes[j] = availableRes[j] + procesi[indeks].allocatedRes[j]; //slobodnim resursima dodaju se oni koji su vec bili posudeni i sad su vraceni
 				}
 
-				if (order == brP)
+				if (order == brP) //
 					break;
 			}
 		}
 
-		if (i == brP - 1)
+		if (i == brP - 1) // ako je i = zadnjem procesu znaci da sve vracamo na pocetak i prolazimo kroz procese
 		{
-			i = 0;
-			brProlazaka++;
+			i = 0; //u ovom slucaju i se stavlja na 0 kako bi krenuli sa vrha liste ponovo
+			brProlazaka++; //broj prolazaka kroz sve procese u listi +1
 		}
 		else
 		{
-			i++;
+			i++; //inace i se povecava za 1 , idemo na sljedeci proces
 		}
 	}
 
-	if (order == brP)
+	if (order == brP) // order iz funkicije nakon svih uspjesno odradenih procesa mora biti jednak broju procesa
 	{
 		printf("\n\n Postoji siguran slijed procesa.");
 	}
 	else
 	{
-		printf("\n\n Ne postoji siguran slijed procesa.");
+		printf("\n\n Ne postoji siguran slijed procesa."); //inace nisu svi obradeni
 	}
 }
 
